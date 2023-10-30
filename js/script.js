d687902c501c87ca3479a79e0f3a9e526cebaaf5
hardevkoder <haroldvaldes@yahoo.com>
2023-10-30 00:21:37 -0500

    Capturo datos de inputs, instancio autos y los muestro en alert

--- js/script.js
+++ js/script.js
@@ -1,32 +1,107 @@
 // ----------------------------------------------------------------------------------------
 // Referencia de elementos del DOM
 // ----------------------------------------------------------------------------------------
-  
+const btnRegistrar = document.getElementById('btnRegistrar');
+const btnMostrar = document.getElementById('btnMostrar');
 
 // ----------------------------------------------------------------------------------------
 // Variables Globales
 // ----------------------------------------------------------------------------------------
-
-
+let codigo = 0;
+let arrayAutos = [];
+let autoRegistrado;
 // ----------------------------------------------------------------------------------------
 // Funciones
 // ----------------------------------------------------------------------------------------
 // Funcion que crea la tarjeta que muestra datos del auto
-const tarjetaDatosAuto = () =>{
+const crearTarjetaDatos = () => {
   const spanResultados = document.getElementById('spanResultados');
   const tarjeta = document.createElement('div');
-  tarjeta.style.width = '200px';
-  tarjeta.style.height = '200px';
-  tarjeta.style.background = 'transparent';
-  tarjeta.style.border = '1px solid silver';
-  tarjeta.style.borderRadius = '10px';
+  tarjeta.classList.add('tarjeta');
   spanResultados.appendChild(tarjeta);
+}
+
+// Funcion que extrae el valor de los inputs y los guarda en un Array
+const extraerValoresInputs = () => {
+  const inputs = document.querySelectorAll('.datosFormulario');
+  const arrayDatos = [];
+  inputs.forEach((input) => {
+    let valor = input.value;
+    arrayDatos.push(valor);
+  });
+  return arrayDatos;
+}
+
+// Funcion Constructora de Objetos (Propiedades Automoviles)
+function Auto(codigo, marca, modelo, color, anio, titular) {
+  this.codigo = codigo;
+  this.marca = marca;
+  this.modelo = modelo;
+  this.color = color;
+  this.anio = anio;
+  this.titular = titular;
+}
+
+// Método para vender el Auto (cambiar el titular)
+Auto.prototype.venderAuto = function (auto, titular) {
+  auto.titular = titular;
+}
+
+// Método para encender el Auto
+Auto.prototype.encender = function (auto) {
+  alert(`El Auto ${auto} está  en marcha`);
+}
+
+// Método para mostrar la información del Auto
+Auto.prototype.verAuto = function (auto) {
+
+}
+
+// Función para instanciar Autos
+const instanciarAuto = (codigo, marca, modelo, color, anio, titular) => {
+  let auto = new Auto(codigo, marca, modelo, color, anio, titular);
+  return auto;
 }
 
 // ----------------------------------------------------------------------------------------
 // Programa Principal
 // ----------------------------------------------------------------------------------------
-for(let i=1;i<=3;i++){
+btnRegistrar.addEventListener('click', () => {
+  // elemento del array de autos (auto instanciado)
+  let elemento='';
 
-  tarjetaDatosAuto();
-}
+  // Incremento el codigo del auto para llevar consecutivo
+  codigo++;
+
+  // Extraigo los valores ingresados en el formulario (desestructuracion)
+  [marca, modelo, color, anio, titular] = extraerValoresInputs();
+
+  // Instancio el auto a registrar
+  autoRegistrado = instanciarAuto(codigo, marca, modelo, color, anio, titular);
+
+  // Agrego la instancia al array de autos
+  arrayAutos.push(autoRegistrado);
+
+  // guardo en variable los autos registrados
+  arrayAutos.forEach((auto) => {
+    elemento += `Codigo: ${auto.codigo}
+    Marca: ${auto.marca}
+    Modelo: ${auto.modelo}
+    Color: ${auto.color}
+    Año: ${auto.anio}
+    Titular: ${auto.titular}
+    \n`;
+    // div.innerHTML = elemento;
+  });
+
+  // Muestro los autos registrados
+  alert(elemento)
+
+});
+
+btnMostrar.addEventListener('click', () => {
+  for (let i = 1; i <= 3; i++) {
+    crearTarjetaDatos();
+  }
+});
+
